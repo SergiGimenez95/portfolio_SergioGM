4fae13bbe5606d647cb053e0222c73f09e919f53
Sergi <sergio.gimenez@mataro.epiaedu.cat>
2024-11-13 17:44:25 +0100

    Add first version for the portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'; // Importa defineConfig
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  // Configuración de Astro
+  site: 'https://tusitio.com',
+  integrations: [tailwind()],
+  // Otras configuraciones
+});
